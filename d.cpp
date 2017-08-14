8f1130b4394e95b3c56433361484682b55fdcdc4
A_titol <ggrkssk@gmail.com>
2017-08-15 01:22:46 +0900

    Add bfs()

--- d.cpp
+++ d.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <cmath>
 #include <vector>
+#include <queue>
 #include <algorithm>
 #include <functional>
 
@@ -12,19 +13,37 @@
 #define dump(x)  cerr << #x << " = " << (x) << endl;
 #define debug(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ")" << " " << __FILE__ << endl;
 
+#define _BFS 1
+// #define _DFS 1
+
 using namespace std;
 typedef long long ll;
+typedef pair<int, ll> pil;
 
 ll cost[100010];
 vector< pair<int, ll> > v[100010];
 
-// void bfs(int x){
-//   FOR(i, 1, n-1){
-//     if(cost[x] != 0) return;
-     
+void bfs(int k){
+  queue< pil > q;
+  q.push(pil(k, 0));
 
-//   }
-// }
+  pil p;
+  int x, y;
+  while(!q.empty()){
+    p = q.front();
+    q.pop();
+
+    x = p.first;
+    REP(i, v[x].size()){
+      y = v[x][i].first;
+      if(cost[y] != -1) continue;
+      cost[y] = p.second + v[x][i].second;
+      q.push(pil(y, cost[y]));
+    }
+
+  }
+}
+
 void dfs(int x, ll c){
   // dump(x);
   // if(cost[x] != -1) {cout << "exit" << endl; return;}
@@ -36,12 +55,13 @@ void dfs(int x, ll c){
   }
 }
 
+#ifdef _BFS
 int main(){
   int n;
   cin >> n;
 
   int x, y, z;
-  pair<int, int> p;
+  pair<int, ll> p;
   REP(i, n-1){
     cin >> x >> y >> z;
     p.first = y;
@@ -55,7 +75,38 @@ int main(){
   cin >> q >> k;
 
   REP(i, n+1) cost[i] = -1;
-  dfs(k, 0);
+  bfs(k);
+
+  REP(i, q){
+    cin >> x >> y;
+    cout << cost[x] + cost[y] << endl;
+  }
+
+  return 0;
+}
+#endif
+
+#ifdef _DFS
+int main(){
+  int n;
+  cin >> n;
+
+  int x, y, z;
+  pair<int, ll> p;
+  REP(i, n-1){
+    cin >> x >> y >> z;
+    p.first = y;
+    p.second = z;
+    v[x].push_back(p);
+    p.first = x;
+    v[y].push_back(p);
+  }
+
+  int q, k;
+  cin >> q >> k;
+
+  REP(i, n+1) cost[i] = -1;
+  dfs(k);
 
   REP(i, q){
     cin >> x >> y;
@@ -66,3 +117,4 @@ int main(){
   
   return 0;
 }
+#endif
